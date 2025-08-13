ad4159bc61e277040792d6621ef28add45aa0afb
nimanew303 <nimanew303@gmail.com>
2025-08-14 00:00:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'SUPREME',
   botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
   botOwner: 'Supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  ownerNumber: '94726800969', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
