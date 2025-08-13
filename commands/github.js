720ef430a54f99cb3e75393d84087aa12beaf4c3
nimanew303 <nimanew303@gmail.com>
2025-08-14 04:38:14 +0530

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -6,12 +6,12 @@ const path = require('path');
 
 async function githubCommand(sock, chatId, message) {
   try {
-    const res = await fetch('https://api.github.com/repos/vinpink2/June-md');
+    const res = await fetch('https://api.github.com/nimanew303/NIMA-V5-new');
     if (!res.ok) throw new Error('Error fetching repository data');
     const json = await res.json();
 
     let txt = 
-           `🔸  \`𝙹𝚄𝙽𝙴 𝙼𝙳 𝚁𝙴𝙿𝙾 𝙸𝙽𝙵𝙾.\` \n\n`;
+           `🔸  \`NIMA-V5 𝙸𝙽𝙵𝙾.\` \n\n`;
     txt += `🔸  *Name* : ${json.name}\n`;
     txt += `🔸  *Watchers* : ${json.watchers_count}\n`;
     txt += `🔸  *Size* : ${(json.size / 1024).toFixed(2)} MB\n`;
@@ -19,8 +19,8 @@ async function githubCommand(sock, chatId, message) {
     txt += `🔸  *REPO* : ${json.html_url}\n`;
     txt += `🔸  *Forks* : ${json.forks_count}\n`;
     txt += `🔸  *Stars* : ${json.stargazers_count}\n`;
-    txt += `🔸  Dont Forget to fork & star⭐ The Repo \n\n`;
-    txt += `> _⎋June Md_`;
+    txt += `🔸  කරුණාකර තරුව rate කිරීමට කරුණාවන්ත වන්න⭐ \n\n`;
+    txt += `> _⎋NIMA-V5_`;
 
     // Use the local asset image
     const imgPath = path.join(__dirname, '../assets/june_repos.jpg');
@@ -28,7 +28,7 @@ async function githubCommand(sock, chatId, message) {
 
     await sock.sendMessage(chatId, { image: imgBuffer, caption: txt }, { quoted: message });
   } catch (error) {
-    await sock.sendMessage(chatId, { text: '❌ Error fetching repository information.' }, { quoted: message });
+    await sock.sendMessage(chatId, { text: '❌ අසාර්ථකයි. නැවත උත්සහ කරන්න.' }, { quoted: message });
   }
 }
 
